65d5f72bca7f5b7da89682205df2ef1bc83d7566
sidmadethis <sidmadethis@gmail.com>
2017-05-17 14:13:13 -0600

    more details on file paths

--- file_reader.py
+++ file_reader.py
@@ -8,3 +8,9 @@ with open('pi_digits.txt') as file_object:
 # you first need to open the file to access it. the open() function needs one argument, the name of the file. python looks for this file in the same directory that the python program is running in.
 
 # the with keyword will close the file once access to it is no longer needed. you could use close() but this can lead to easy mistakes
+
+# if this file wasn't in the same directory as the python program, you could give a relative file path.
+
+# with open('text_files/filename.txt') as file_object
+
+# Or use the full file path if needed
