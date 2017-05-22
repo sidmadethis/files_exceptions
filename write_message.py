c45087733e45bf4b77d4123f6c927d7b74ee9f79
sidmadethis <sidmadethis@gmail.com>
2017-05-22 09:40:47 -0600

    appending multiple lines to a file

--- write_message.py
+++ write_message.py
@@ -1,7 +1,10 @@
 filename = 'programming.txt'
 
 with open(filename, 'w') as file_object:
-    file_object.write("i love programming")
+    file_object.write("i love programming.\n")
 
 
 # w is write mode, r is read mode, a is append mode
+with open(filename, 'a') as file_object:
+    file_object.write("I also love to use big data.\n")
+    file_object.write("i love creating apps that can be run in a browser.\n")
