2633dbea11734a4a8b206ef6f8b13bfa152d23ad
sidmadethis <sidmadethis@gmail.com>
2017-05-23 23:56:23 -0600

    if else block with try except

--- division.py
+++ division.py
@@ -1,7 +1,28 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("you can't divide by zero")
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("you can't divide by zero")
 
 
 # if the code inside the try block works, python will skip over the except block.
+
+
+print("give me two numbers and I will divide them")
+print("\nEnter q to quit")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("\nEnter second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number)/ int(second_number)
+    except ZeroDivisionError:
+        print("you can't divide by zero")
+    else:
+        print(answer)
+
+
+# you can wrap the line that might cause errors in a try/except/else block
